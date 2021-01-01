35afeb774ae3f2f9dd575b1146a5991dcd0c3d9d
Solar <xviii.solar@gmail.com>
2021-01-01 01:07:35 -0500

    fixing help command

--- commands/help.js
+++ commands/help.js
@@ -10,18 +10,26 @@ module.exports = {
         const { commands } = message.client;
         const command = commands.get(args[0]) || commands.find(c => c.aliases && c.aliases.includes(args[0]));
         if (!args.length || !command) {
-          return message.channel.send(`**Commands List:**\n\`\`\`${commands.map(command => command.n).join(', ')}\`\`\`\nFor help with a specific command, type \`%help <command>\`!`);
+          const helpEmbed = new Discord.MessageEmbed();
+          helpEmbed.setColor(0xF097DF);
+          helpEmbed.setTitle(`%help`);
+  	      helpEmbed.addField('Commands', commands.map(command => command.n).join(', '));
+  	      helpEmbed.addField('\u200b', '\u200b');
+  	      helpEmbed.addField('', `For help with a specific command, type moo!<command>`);
+  	      helpEmbed.setTimestamp();
+  	      helpEmbed.setFooter(`I'm ${client.user.tag}!`, client.user.avatarURL);
+          return message.channel.send({helpEmbed});
         }
 
-        const embed = new Discord.RichEmbed()
-    .setColor(0x000000)
-    .setTitle(`%help`)
-  	.addField('Command Name (Aliases)', `${command.n} (${command.a.join(', ')})`)
-  	.addBlankField()
-  	.addField('Description', command.d, true)
-  	.addField('Usage', command.u, true)
-  	.setTimestamp()
-  	.setFooter(`I'm ${client.user.tag}!`, client.user.avatarURL);
+    const embed = new Discord.MessageEmbed();
+    embed.setColor(0xF097DF);
+    embed.setTitle(`%help`);
+  	embed.addField('Command Name (Aliases)', `${command.n} (${command.a.join(', ')})`);
+  	embed.addField('\u200b', '\u200b');
+  	embed.addField('Description', command.d, true);
+  	embed.addField('Usage', command.u, true);
+  	embed.setTimestamp();
+  	embed.setFooter(`I'm ${client.user.tag}!`, client.user.avatarURL);
     message.channel.send({embed});
 	},
 };
