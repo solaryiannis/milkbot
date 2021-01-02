bb4ee9634c01caec847f10ae94d798b478de1022
Solar <xviii.solar@gmail.com>
2021-01-01 22:05:04 -0500

    bug fixing

--- commands/help.js
+++ commands/help.js
@@ -4,24 +4,24 @@ module.exports = {
 	n: 'help',
 	a: ['help', 'h', 'command', 'commands', 'c'],
     d: 'It\'s time to help!',
-    u: 'moo!help or moo!help <command>',
+    u: '%help or %help <command>',
     
     async execute(client, message, args) {
-        const { commands } = message.guild.client;
+        const { commands } = message.client;
         const command = commands.get(args[0]) || commands.find(c => c.aliases && c.aliases.includes(args[0]));
         if (!args.length || !command) {
           return message.channel.send(`**Commands List:**\n\`\`\`${commands.map(command => command.n).join(', ')}\`\`\`\nFor help with a specific command, type \`moo!help <command>\`!`);
         }
 
-    const embed = new Discord.MessageEmbed();
-    embed.setColor(0xF097DF);
-    embed.setTitle(`%help`);
-  	embed.addField('Command Name (Aliases)', `${command.n} (${command.a.join(', ')})`);
-  	embed.addField('\u200b', '\u200b');
-  	embed.addField('Description', command.d, true);
-  	embed.addField('Usage', command.u, true);
-  	embed.setTimestamp();
-  	embed.setFooter(`I'm ${message.guild.client.user.tag}!`, message.guild.client.user.avatarURL());
-    message.channel.send({embed});
+        const embed = new Discord.MessageEmbed();
+        embed.setColor(0xfca2e4);
+        embed.setTitle(`moo!help`);
+        embed.addField('Command Name (Aliases)', `${command.n} (${command.a.join(', ')})`);
+        embed.addField('\u200b', '\u200b');
+        embed.addField('Description', command.d, true);
+        embed.addField('Usage', command.u, true);
+        embed.setTimestamp();
+        embed.setFooter(`I'm ${message.client.user.tag}!`, message.client.user.avatarURL());
+        message.channel.send({embed});
 	},
 };
