69513dab8da375e18bed641ce680b4d6820c8c53
Solar <xviii.solar@gmail.com>
2021-01-09 13:43:49 -0500

    sleep

---
+++ commands/sleepy.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    n: 'sleepy',
+    a: ['sleepy'],
+    d: 'Let Milk sleep!',
+    u: 'moo!sleepy',
+      
+    async execute(client, message, args) {
+      message.channel.send(`https://cdn.discordapp.com/attachments/792875009462763540/797245070235533312/20201218_155750.png`);
+    },
+  };
